6f458da0e0bd2e087e7aa859cf379a80675ff2ac
Anastassia Varabei <nastenkaok1984@gmail.com>
2020-07-26 23:15:29 +0300

    modified datedesc.component

--- AngularTest/src/app/datedesc/datedesc.component.ts
+++ AngularTest/src/app/datedesc/datedesc.component.ts
@@ -7,6 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DatedescComponent implements OnInit {
 
+  days = [
+    {day: 'пт', date: '12'},
+    {day: 'сб', date: '13'},
+    {day: 'вс', date: '14'},
+    {day: 'пн', date: '15'},
+    {day: 'вт', date: '16'},
+    {day: 'ср', date: '17'},
+    {day: 'чт', date: '18'}
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
